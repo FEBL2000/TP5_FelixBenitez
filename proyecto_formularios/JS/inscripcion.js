e893359c6efec9920d73c5bff61a3a6443ded54b
FEBL2000 <febl1313@gmail.com>
2025-11-21 00:41:12 -0300

    Delete proyecto_formularios directory

--- proyecto_formularios/JS/inscripcion.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("formInscripcion").addEventListener("submit", function(e) {
-  e.preventDefault();
-  let nombre = document.getElementById("nombre").value;
-  let correo = document.getElementById("correo").value;
-  alert(`Inscripción recibida:\nNombre: ${nombre}\nCorreo: ${correo}`);
-});
